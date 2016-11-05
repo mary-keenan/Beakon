2bf572ef5dc874af9a9c76c8e133575a03cce33c
Erica Lee <erica.lee@students.olin.edu>
2016-11-05 17:19:43 -0400

    ADD:Empty methods to FirebaseHandler

---
+++ Beakon/app/src/main/java/erica/beakon/FirebaseHandler.java
@@ -0,0 +1,35 @@
+package erica.beakon;
+
+
+public class FirebaseHandler {
+    String url;
+
+    public FirebaseHandler(String firebaseURL) {
+        this.url = firebaseURL;
+    }
+
+    public void addUser(User user) {
+
+    }
+
+    public void addMovement(Movement movement) {
+
+    }
+
+    public String getUserRef(){
+
+    }
+
+    public String getMovementRef() {
+
+    }
+
+    public void addUsertoMovement(User user, Movement movement) {
+
+    }
+
+    public void removeUserfromMovement(User user, Movment movement) {
+
+    }
+
+}
