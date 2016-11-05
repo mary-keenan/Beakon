436a9df0736d5c62cc853fd5cd30aa89d82ede0e
Erica Lee <erica.lee@students.olin.edu>
2016-11-05 18:16:17 -0400

    ADD: implemented proof of concept addUser and getUserRef method

--- Beakon/app/src/main/java/erica/beakon/FirebaseHandler.java
+++ Beakon/app/src/main/java/erica/beakon/FirebaseHandler.java
@@ -1,35 +1,44 @@
 package erica.beakon;
 
 
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FirebaseHandler {
-    String url;
+    FirebaseDatabase db;
+    DatabaseReference ref;
 
-    public FirebaseHandler(String firebaseURL) {
-        this.url = firebaseURL;
+    public FirebaseHandler(FirebaseDatabase db, DatabaseReference ref) {
+        this.ref = ref;
+        this.db = db;
     }
 
-    public void addUser(User user) {
-
+    public void addUser(String user, String value) {
+        ref.child(user).setValue(value);
     }
 
-    public void addMovement(Movement movement) {
-
+//    public void addMovement(Movement movement) {
+//
+//    }
+//
+    public DatabaseReference getUserRef(){
+        DatabaseReference userRef = ref.child("user").getRef();
+        Log.d("***", userRef.toString());
+        return userRef;
     }
-
-    public String getUserRef(){
-
-    }
-
-    public String getMovementRef() {
-
-    }
-
-    public void addUsertoMovement(User user, Movement movement) {
-
-    }
-
-    public void removeUserfromMovement(User user, Movment movement) {
-
-    }
+//
+//    public DatabaseReference getMovementRef() {
+//
+//    }
+//
+//    public void addUsertoMovement(User user, Movement movement) {
+//
+//    }
+//
+//    public void removeUserfromMovement(User user, Movment movement) {
+//
+//    }
 
 }
