df83c775ec37e0fe495325ab867a6e65fee30747
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2016-11-06 11:24:09 -0500

    ADD fragment code for main acivitiy

--- Beakon/app/src/main/java/erica/beakon/MainActivity.java
+++ Beakon/app/src/main/java/erica/beakon/MainActivity.java
@@ -1,13 +1,27 @@
 package erica.beakon;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "MainActivity";
+    FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+    }
+
+    private void startFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = this.fragmentManager.beginTransaction();
+//        fragmentTransaction.setCustomAnimations(R.animator.enter_from_left, R.animator.exit_to_right);
+        fragmentTransaction.replace(R.id.fragment_holder, fragment, "");
+        fragmentTransaction.commit();
     }
 }
