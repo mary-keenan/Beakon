cfd1033b52b098abee4c1d3bc4442944ef524085
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2016-11-06 15:47:41 -0500

    ADD: tabs

--- Beakon/app/src/main/java/erica/beakon/MainActivity.java
+++ Beakon/app/src/main/java/erica/beakon/MainActivity.java
@@ -1,10 +1,12 @@
 package erica.beakon;
 
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fragmentManager = getSupportFragmentManager();
+        final ViewPagerAdapter pagerAdapter;
+        final ViewPager pager;
 
-        startFragment(new MyMovementsTab());
-    }
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-    private void startFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = this.fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_holder, fragment, "");
-        fragmentTransaction.commit();
+        pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        final Button myMovementsButton = (Button)findViewById(R.id.my_movements);
+        final Button suggestedMovementsButton = (Button)findViewById(R.id.movements);
+        myMovementsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pager.setCurrentItem(0);
+                myMovementsButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+                suggestedMovementsButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+            }
+        });
+
+        suggestedMovementsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pager.setCurrentItem(ViewPagerAdapter.NUM_PAGES-1);
+                suggestedMovementsButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+                myMovementsButton.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+            }
+        });
+
     }
 }
--- Beakon/app/src/main/java/erica/beakon/MovementsTab.java
+++ Beakon/app/src/main/java/erica/beakon/MovementsTab.java
@@ -9,14 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MovementsTab.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MovementsTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MovementsTab extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +20,7 @@ public class MovementsTab extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+//    private OnFragmentInteractionListener mListener;
 
     public MovementsTab() {
         // Required empty public constructor
@@ -67,29 +60,29 @@ public class MovementsTab extends Fragment {
         return inflater.inflate(R.layout.fragment_movements_tab, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
 
     /**
      * This interface must be implemented by activities that contain this
@@ -101,8 +94,8 @@ public class MovementsTab extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
 }
