c86316d23e01fb0016f3482b56b296ee471ab870
mary-keenan <mary.keenan@students.olin.edu>
2016-12-15 21:15:12 -0500

    double-tap implemented; works perfectly on emulator and shitty phone

--- Beakon/app/src/main/java/erica/beakon/Adapters/MyMovementAdapter.java
+++ Beakon/app/src/main/java/erica/beakon/Adapters/MyMovementAdapter.java
@@ -29,6 +29,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import junit.framework.Test;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -47,66 +49,52 @@ public class MyMovementAdapter extends MovementAdapter {
     String databaseURL = "https://beakon-5fa96.firebaseio.com/";
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
     private DatabaseReference ref = db.getReferenceFromUrl(databaseURL);
-    FirebaseHandler firebaseHandler = new FirebaseHandler(db,ref);
+    FirebaseHandler firebaseHandler = new FirebaseHandler(db, ref);
     User currentUser;
     private long thisTime = 0;
     private long prevTime = 0;
     private boolean firstTap = true;
-    protected static final long DOUBLE_CLICK_MAX_DELAY = 1000L;
+    protected static final long DOUBLE_CLICK_MAX_DELAY = 200L;
 
     public MyMovementAdapter(Context context, ArrayList<Movement> movements) {
         super(context, movements, R.layout.my_movement_item);
 
     }
 
-    protected void setUpView(final MainActivity activity, final Movement movement, final View convertView,final int position) {
+    protected void setUpView(final MainActivity activity, final Movement movement, final View convertView, final int position) {
         currentUser = activity.getCurrentUser();
 
         Button deleteBtn = (Button) convertView.findViewById(R.id.deleteButton);
 
-        //http://stackoverflow.com/questions/6716224/ontap-listener-implementation
+        //http://stackoverflow.com/questions/4804798/doubletap-in-android
         convertView.findViewById(R.id.card_view_layout).setOnTouchListener(new View.OnTouchListener() {
+
+            private GestureDetector gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onDoubleTap(MotionEvent e) {
+                    if (currentUser!= null) {
+                        if (currentUser.getMovements().get(movement.getId()).get("status") != true) {
+                            Toast.makeText(activity, "Completed!", Toast.LENGTH_SHORT).show();
+                            firebaseHandler.setMovementofUserStatus(currentUser, movement, true);
+                            currentUser.updateMovements(movement.getId(), true);
+                            notifyDataSetChanged();
+                        } else {
+                            Toast.makeText(activity, "Not completed.", Toast.LENGTH_SHORT).show();
+                            firebaseHandler.setMovementofUserStatus(currentUser, movement, false);
+                            currentUser.updateMovements(movement.getId(), false);
+                            notifyDataSetChanged();
+                        }
+                    }
+                    return super.onDoubleTap(e);
+                }
+                // implement here other callback methods like onFling, onScroll as necessary
+            });
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(firstTap){
-                    thisTime = SystemClock.uptimeMillis();
-                    firstTap = false;
-                }
-                else
-                {
-                    prevTime = thisTime;
-                    thisTime = SystemClock.uptimeMillis();
-                    //Check that thisTime is greater than prevTime just incase system clock reset to zero
-                    if(thisTime > prevTime){
-                        //Check if times are within our max delay
-                        if((thisTime - prevTime) <= DOUBLE_CLICK_MAX_DELAY){
-                            //We have detected a double tap!
-                            if (currentUser!= null) {
-                                Log.d("GETTING MOVEMENT", String.valueOf(currentUser.getMovements().get(movement.getId()).get("status")));
-                                if (currentUser.getMovements().get(movement.getId()).get("status") != true){
-                                    Toast.makeText(activity, "Completed!", Toast.LENGTH_SHORT).show();
-                                    firebaseHandler.setMovementofUserStatus(currentUser, movement, true);
-                                    currentUser.updateMovements(movement.getId(), true);
-                                    notifyDataSetChanged();
-                                }
-                                else {
-                                    Toast.makeText(activity, "Not completed.", Toast.LENGTH_SHORT).show();
-                                    firebaseHandler.setMovementofUserStatus(currentUser, movement, false);
-                                    currentUser.updateMovements(movement.getId(), false);
-                                    notifyDataSetChanged();
-                                }
-                            }
-                        }
-                        else {
-                            //Otherwise Reset firstTap
-                            firstTap = true;
-                        }
-                    }
-                    else {
-                        firstTap = true;
-                    }
-                }
-                return false;
+//                Log.d("TEST", "Raw event: " + event.getAction() + ", (" + event.getRawX() + ", " + event.getRawY() + ")");
+                gestureDetector.onTouchEvent(event);
+                return true;
             }
         });
 
@@ -145,6 +133,7 @@ public class MyMovementAdapter extends MovementAdapter {
             @Override
             public void onCancelled(DatabaseError databaseError) {}
         });
+
 
         //final Movement finalMovement = movement;
 //        checkBox.setOnClickListener(new View.OnClickListener() {
